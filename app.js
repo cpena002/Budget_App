cd035e05c5eb6a8c94e4e62121308113b823d2a7
cpena002 <cpena002@yahoo.com>
2018-10-23 22:31:13 -0700

    set up budget, UI, and data controller

---
+++ app.js
@@ -0,0 +1,15 @@
+// BUDGET CONTROLLER
+var budgetController = (function(){
+  // TO DO
+
+})();
+
+var UIController = (function(){
+  // TO DO
+
+})();
+
+var controller = (function(budgetCtrl, UICtrl){
+  // TO DO
+
+})(budgetController, UIController)
