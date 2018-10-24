9ceeac528950b22dd9647d8b68433fd62421c5d6
cpena002 <cpena002@yahoo.com>
2018-10-23 23:11:38 -0700

    attached event listener function to the add  button

--- app.js
+++ app.js
@@ -4,12 +4,35 @@ var budgetController = (function(){
 
 })();
 
+// UI CONTROLLER
 var UIController = (function(){
   // TO DO
 
 })();
 
-var controller = (function(budgetCtrl, UICtrl){
-  // TO DO
+// GLOBAL APP CONTROLLER
+var globalController = (function(budgetCtrl, UICtrl){
+  var ctrlAddItem = function(){
+    // 1. get the field input data
+
+    // 2. ad dthe item to the budget controller.
+
+    // 3. Add the item to the UI
+
+    // 4. Calculate the budget.
+
+    // 5. Display the budget on the UI
+    console.log('it works');
+  }
+
+  //Event Listeners
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+  document.addEventListener('keypress', function(event){
+    if(event.keyCode === 13 || event.which === 13){
+      ctrlAddItem();
+    }
+
+  });
 
 })(budgetController, UIController)
