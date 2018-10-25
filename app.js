0f2ce5b0518792bcd9abe855481ccb909e343fca
cpena002 <cpena002@yahoo.com>
2018-10-24 20:52:36 -0700

    set up selectors to extract user input on income/expenses

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 // BUDGET CONTROLLER
 var budgetController = (function(){
-  // TO DO
+  // functions included in the object returned by this module are poublic and accessible to other modules.
 
 })();
 
 // UI CONTROLLER
 var UIController = (function(){
-  // TO DO
+  // functions included in the object returned by this module are poublic and accessible to other modules.
+
+  return {
+    getInput: function(){
+      //Get the type, description, and value enered by the user.
+      return {
+        /* We could have stored the values in a varialbes, but in order to return 3 values within the same object is easier to have the object return another object (the one we're in) */
+        type: document.querySelector('.add__type').value, // Will either be inc or exp
+        description: document.querySelector('.add__description').value,
+        value: document.querySelector('.add__value').value
+      }
+    }
+  }
+
+
 
 })();
 
@@ -14,7 +28,8 @@ var UIController = (function(){
 var globalController = (function(budgetCtrl, UICtrl){
   var ctrlAddItem = function(){
     // 1. get the field input data
-
+    var input = UICtrl.getInput();
+    console.log(input);
     // 2. ad dthe item to the budget controller.
 
     // 3. Add the item to the UI
@@ -22,7 +37,7 @@ var globalController = (function(budgetCtrl, UICtrl){
     // 4. Calculate the budget.
 
     // 5. Display the budget on the UI
-    console.log('it works');
+
   }
 
   //Event Listeners
