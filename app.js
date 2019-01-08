f49a6aa6ed74ec5ec93e20bfdfd908ffc49e6317
cpena002 <cpena002@yahoo.com>
2019-01-07 22:19:03 -0800

    Updated UI Controller to add new items to the UI

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ var budgetController = (function(){
     this.id = id;
     this.description = description;
     this.value = value;
-  }
+  };
 
   // Income function constructor.
   var Income = function(id, description, value){
     this.id = id;
     this.description = description;
     this.value = value;
-  }
+  };
 
   var data = {
     allItems: {
@@ -25,7 +25,7 @@ var budgetController = (function(){
       expenses: 0,
       incomces: 0
     }
-  }
+  };
 
   return {
     addItem: function(type, des, val){
@@ -55,7 +55,7 @@ var budgetController = (function(){
     testing: function (){
       console.log(data);
     }
-  }
+  };
 
 })();
 
@@ -67,7 +67,9 @@ var UIController = (function(){
     inputType: '.add__type',
     inputDescription: '.add__description',
     inputValue: '.add__value',
-    inputButton: '.add__btn'
+    inputButton: '.add__btn',
+    incomeContainer: '.income__list',
+    expensesContainer: '.expenses__list'
   }
 
   return {
@@ -79,6 +81,25 @@ var UIController = (function(){
         description: document.querySelector(DOMStrings.inputDescription).value,
         value: document.querySelector(DOMStrings.inputValue).value
       }
+    },
+
+    addListItem: function(obj, type){
+      var html, newHtml, element;
+      // Create HTML string with place holer text.
+      if (type === 'income'){
+        element = DOMStrings.incomeContainer;
+        html = '<div class="item clearfix" id="income-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>'
+      } else if (type === 'expense') {
+        element = DOMStrings.expensesContainer;
+        html = '<div class="item clearfix" id="expense-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">- %value%</div> <div class="item__percentage">21%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>'
+      }
+      // Replace placeholder text with actual data.
+      newHtml = html.replace('%id%', obj.id);
+      newHtml = newHtml.replace('%description%', obj.description);
+      newHtml = newHtml.replace('%value%', obj.value);
+      // Insert HTML into the DOM.
+      document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+
     },
 
     getDOMStrings: function (){
@@ -111,7 +132,7 @@ var globalController = (function(budgetCtrl, UICtrl){
     // 2. add the item to the budget controller.
     newItem = budgetCtrl.addItem(input.type, input.description, input.value);
     // 3. Add the item to the UI
-
+    UICtrl.addListItem(newItem, input.type);
     // 4. Calculate the budget.
 
     // 5. Display the budget on the UI
