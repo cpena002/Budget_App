c74b3e845ec8d47e31c83ebbb38263300f755fc7
cpena002 <cpena002@yahoo.com>
2019-01-08 21:52:13 -0800

    set up method in globalController that will calculate, return, and display the budget in the UI.

--- app.js
+++ app.js
@@ -139,6 +139,14 @@ var globalController = (function(budgetCtrl, UICtrl){
 
   };
 
+  var updateBudget = function(){
+    // 1. Calculate the budget.
+
+    // 2. Return the budget.
+
+    // 3. Display the budget on the UI
+
+  }
 
   var ctrlAddItem = function(){
     var input, newItem;
@@ -150,9 +158,8 @@ var globalController = (function(budgetCtrl, UICtrl){
     UICtrl.addListItem(newItem, input.type);
     // 4. Clear the fields.
     UICtrl.clearFields();
-    // 5. Calculate the budget.
-
-    // 6. Display the budget on the UI
+    // 5. Calculate and update budget.
+    updateBudget();
 
   };
 
