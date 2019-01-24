9e82e5d632d880ae5349d15edd24ee90d1f691eb
cpena002 <cpena002@yahoo.com>
2019-01-23 21:32:19 -0800

    update the UIController with a methods that turns values entered by users into decimas and adds commas at the thousands place.

--- app.js
+++ app.js
@@ -242,6 +242,24 @@ var UIController = (function(){
       });
     },
 
+    formatNumber: function(num, type){
+      var numSplit;
+
+      num = Math.abs(num);
+      num = num.toFixed(2);
+
+      numSplit = num.split('.');
+
+      int = numSplit[0];
+      // Add
+      if(int.length > 3){
+        int = int.substr(0, int.length - 3) + ',' + int.substr(int.length - 3, 3);
+      };
+
+      dec = numSplit[1];
+      return type === 'exp' ? '-' : '+' + ' ' + int + dec;
+    }
+
     getDOMStrings: function (){
       return DOMStrings
     }
